a5e463d5396048ed4e86d2f7a7396819e1baacb6
MathiasClari <mathiasclari@noximity.com>
2023-12-26 01:16:54 +0100

    Rename package from 'reefrealm' to 'cherrycraft'
    In multiple files, the package name has been renamed from 'net.reefrealm.reefrealmskypvp' to 'net.cherrycraft.cherryskypvp'. This rename also involves adjusting class dependencies and import statements. Adaptions for these changes are made in files like TradeSystem.java, ReefRealmSkyPVP.java, etc. This commit is part of broader modifications to rebrand the project.

---
+++ src/main/java/net/cherrycraft/cherryskypvp/spawn/SetSpawnCommand.java
@@ -0,0 +1,35 @@
+package net.cherrycraft.cherryskypvp.spawn;
+
+import net.cherrycraft.cherryskypvp.CherrySkyPVP;
+import net.cherrycraft.cherryskypvp.manager.CommandManager;
+import org.bukkit.Location;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class SetSpawnCommand extends CommandManager {
+    public SetSpawnCommand(String commandName) {
+        super("setspawn");
+        setPermission("reefrealmskypvp.setspawn");
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("This command can only be executed by a player.");
+            return;
+        }
+
+        Player player = (Player) sender;
+        Location location = player.getLocation();
+
+        CherrySkyPVP.getInstance().getConfig().set("spawn.world", location.getWorld().getName());
+        CherrySkyPVP.getInstance().getConfig().set("spawn.x", location.getX());
+        CherrySkyPVP.getInstance().getConfig().set("spawn.y", location.getY());
+        CherrySkyPVP.getInstance().getConfig().set("spawn.z", location.getZ());
+        CherrySkyPVP.getInstance().getConfig().set("spawn.yaw", location.getYaw());
+        CherrySkyPVP.getInstance().getConfig().set("spawn.pitch", location.getPitch());
+
+        CherrySkyPVP.getInstance().saveConfig();
+        player.sendMessage("Spawn location saved.");
+    }
+}
